fdeaa09759239ba8135bddc3e62894c30cf5e87d
tpoeiras <thiagopoeirassilva@gmail.com>
2021-08-02 14:34:58 -0300

    Commit 6

--- src/Rental.java
+++ src/Rental.java
@@ -37,4 +37,14 @@ public class Rental {
 
         return thisAmount;
    }
+
+   public int getFrequentRenterPoints() {
+        // add bonus for a two day new release rental
+        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            this.getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+   }
 }
